7103c4e124c615374322f32a99f4942bd72f3c89
Adam Matthews <52178922+adamsthws@users.noreply.github.com>
2024-01-04 14:05:42 +0000

    Added retry to user_id and added check for cURL

--- cloudflare-ddns-updater.sh
+++ cloudflare-ddns-updater.sh
@@ -30,7 +30,7 @@ if source "$script_dir/.env"; then
     if ! [[ " ${debug_level_allowed[*]} " =~ " $DEBUG_LEVEL " ]]; then
         error "Invalid DEBUG_LEVEL: '$DEBUG_LEVEL'. Must be one of: ${debug_level_allowed[*]}."
     else
-        debug "Check 1 (of 9) passed. .env file loaded."
+        debug "Check 1 (of 10) passed. .env file loaded."
     fi
 else
     error "Error: failed to source file: $script_dir/.env"
@@ -46,16 +46,27 @@ fi
 if ps ax | grep "$0" | grep -v "$$" | grep bash | grep -v grep > /dev/null; then
     error "Error: The script is already running."
 else
-    debug "Check 2 (of 9) passed. Script is not already running."
+    debug "Check 2 (of 10) passed. Script is not already running."
 fi
 
 # Check if jq is installed
-check_jq=$(which jq)
-if [ -z "${check_jq}" ]; then
+if ! command -v jq >/dev/null 2>&1; then
     error "Error: jq is not installed."
 else
-    debug "Check 3 (of 9) passed. 'jq' is installed."
+    debug "Check 3 (of 10) passed. 'jq' is installed."
 fi
+
+# Check if cURL is installed
+if ! command -v curl >/dev/null 2>&1; then
+    error "Error: cURL is not installed."
+else
+    debug "Check 4 (of 10) passed. 'cURL' is installed."
+fi
+
+# Set cURL parameters
+curl_timeout=10  # How many seconds before cURL times out
+curl_retries=3   # Maximum number of retries
+curl_wait=5      # Seconds to wait between retries
 
 # Check the subdomain
 # Check if the dns_record field (subdomain) contains dot
@@ -64,7 +75,7 @@ if [[ $DNS_RECORD == *.* ]]; then
     if [[ $DNS_RECORD != *.$ZONE_NAME ]]; then
         error "Error: The Zone in DNS_RECORD does not match the defined Zone in ZONE_NAME."
     else
-        debug "Check 4 (of 9) passed. DNS zone to check/update: $DNS_RECORD."
+        debug "Check 5 (of 10) passed. DNS zone to check/update: $DNS_RECORD."
     fi
 # check if the dns_record (subdomain) is not complete and contains invalid characters
 elif ! [[ $DNS_RECORD =~ ^[a-zA-Z0-9-]+$ ]]; then
@@ -72,21 +83,31 @@ elif ! [[ $DNS_RECORD =~ ^[a-zA-Z0-9-]+$ ]]; then
 # if the dns_record (subdomain) is not complete, complete it
 else
     DNS_RECORD="$DNS_RECORD.$ZONE_NAME"
-    debug debug "Check 4 (of 9) passed. DNS zone to check/update: $DNS_RECORD."
+    debug debug "Check 5 (of 10) passed. DNS zone to check/update: $DNS_RECORD."
 fi
 
-# Get Cloudflare User ID
-user_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
-            -H "Authorization: Bearer $API_KEY" \
-            -H "Content-Type:application/json" \
-            | jq -r '{"result"}[] | .id'
-        )
-# Check if the API is valid and the email is correct
-if [ $user_id ]; then
-    debug "Check 5 (of 9) passed. Cloudflare User ID is: $user_id."
+# Attempt to obtain the Cloudflare User ID.
+user_id=""
+for (( i=0; i<curl_retries; i++ )); do
+    user_id=$(curl -s -m "$curl_timeout" \
+                -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
+                -H "Authorization: Bearer $API_KEY" \
+                -H "Content-Type: application/json" \
+                | jq -r '.result | .id')
+    if [ -n "$user_id" ]; then
+        break # Exit loop if user_id is obtained
+    fi
+    # Retry if unsuccessful.
+    sleep "$curl_wait"
+done
+
+# Check if User ID has been obtained sucessfully
+if [ -n "$user_id" ]; then
+    debug "Check 6 (of 10) passed. Cloudflare User ID is: $user_id."
 else
     error "Error: There is a problem with the Cloudflare API token."
 fi
+
 
 # Get Cloudflare Zone ID
 zone_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$ZONE_NAME&status=active" \
@@ -97,7 +118,7 @@ zone_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$ZONE_
         )
 # Check if the Zone ID is avilable
 if [ $zone_id ]; then
-    debug "Check 6 (of 9) passed. Cloudflare Zone ID is: $zone_id."
+    debug "Check 7 (of 10) passed. Cloudflare Zone ID is: $zone_id."
 else
     error "Error: There is a problem with getting the Zone ID (sub-domain) or the email address (username)."
 fi
@@ -111,7 +132,7 @@ dns_record_a_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zo
 dns_record_a_ip=$(echo $dns_record_a_id |  jq -r '{"result"}[] | .[0] | .content')
 # Check if the IP can be retrieved via API
 if [ $dns_record_a_ip ]; then
-    debug "Check 7 (of 9) passed. DNS Zone A-record IP (via Cloudflare API) is: $dns_record_a_ip."
+    debug "Check 8 (of 10) passed. DNS Zone A-record IP (via Cloudflare API) is: $dns_record_a_ip."
 else
     error "Error: There is a problem with getting the zone A record IP via Cloudflare API."
 fi
@@ -122,7 +143,7 @@ check_record_ipv4=$(dig -t a +short ${DNS_RECORD} | tail -n1)
 if [ -z "${check_record_ipv4}" ]; then
     error "Error: No A Record is setup for ${DNS_RECORD}."
 else
-    debug "Check 8 (of 9) passed. DNS zone A-record IP (via 'dig' domain groper): $check_record_ipv4."
+    debug "Check 9 (of 10) passed. DNS zone A-record IP (via 'dig' domain groper): $check_record_ipv4."
 fi
 
 # Get the machine's WAN IP
@@ -142,7 +163,7 @@ valid_ipv4='^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]
 if ! [[ "$machine_ipv4" =~ $valid_ipv4 ]]; then
     error "Error: IP Address returned was invalid: '$machine_ipv4'"
 else
-    debug "Check 9 (of 9) passed. Machine's public (WAN) IP is: $machine_ipv4."
+    debug "Check 10 (of 10) passed. Machine's public (WAN) IP is: $machine_ipv4."
 fi
 
 # Check if the machine's IPv4 is different to the Cloudflare IPv4
